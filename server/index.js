07e451506c1b8b1a13a368a3dc8014f9cfb7ca06
Kishor S <kishorsbca@gmail.com>
2024-04-02 10:19:05 +0530

    added models and push data into db

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // Data Import
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 // CONFIGURATION
 dotenv.config();
@@ -40,5 +42,7 @@ mongoose
 
     // Add Data One Time
     // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
   })
   .catch((error) => console.log(`${error} didn't connect`));
