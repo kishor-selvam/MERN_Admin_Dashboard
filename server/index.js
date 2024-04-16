ead25a464840def57155d70a200efbf5a79547d8
Kishor S <kishorsbca@gmail.com>
2024-04-16 18:06:56 +0530

    Performance Module has been completed

--- server/index.js
+++ server/index.js
@@ -15,13 +15,15 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import OverallStat from './models/OverallStat.js'
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
-  dataOverallStat
+  dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 // CONFIGURATION
@@ -54,5 +56,6 @@ mongoose
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(`${error} didn't connect`));
